56e4a86ff0380703665baefbbcec51dffb1c9df7
Kiran-Hosur <kiranvhosur@gmail.com>
2019-11-23 12:08:25 +0530

    start planet sim

---
+++ Kivy_examples/Calculator/planet_sim1.py
@@ -0,0 +1,7 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.properties import (NumericProperty, ReferenceListProperty, ObjectProperty,StringProperty)
